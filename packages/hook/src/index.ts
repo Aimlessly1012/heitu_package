5dd324d9bb810f6c5ca0fcff15365b53b1f4787c
Aimlessly1012 <444769150@qq.com>
2024-07-17 10:52:32 +0800

    feat: 初始化..

--- packages/hook/src/index.ts
+++ packages/hook/src/index.ts
@@ -1,2 +1,2 @@
-export { default as useUpdateEffect } from './useUpdateEffect';
+// export { default as useUpdateEffect } from './useUpdateEffect';
 export { default as useTest } from './useTest';
--- packages/hook/src/useUpdateEffect/index.ts
+++
@@ -1,5 +0,0 @@
-import { useEffect, useRef } from 'react';
-
-const useUpdateEffect = hook => {};
-
-export default useUpdateEffect;
